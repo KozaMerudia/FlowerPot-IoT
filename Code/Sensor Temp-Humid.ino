34412b4e4db88c0955f862256b935559e63c6478
Janet Hadjiangelova <janet.hadjiangelova@gmail.com>
2021-04-08 22:39:10 +0300

    T/H + LCD

--- Code/Sensor Temp-Humid.ino
+++ Code/Sensor Temp-Humid.ino
@@ -1,8 +1,11 @@
 #include <dht.h>
 #include <LiquidCrystal.h>
+
 //LiquidCrystal lcd(8, 9, 2, 3, 4, 5);
+
 const int rs = 8, en = 9, d2 = 2, d3 = 3, d4 = 4, d5 = 5;
 LiquidCrystal lcd(rs, en, d2, d3, d4, d5);
+
 #define dht_apin 12  
 
 dht DHT;
@@ -12,7 +15,6 @@ void setup(){
   Serial.begin(9600);
   
   lcd.begin(8, 2);
-  lcd.clear();
   lcd.setCursor(0,0); 
   lcd.print("proba");
   
@@ -24,15 +26,27 @@ void setup(){
  
 void loop(){
   //Start of Program 
+
     DHT.read11(dht_apin);
-  
-    Serial.print("Current humidity = ");
+    
+    Serial.print("Humidity = ");
     Serial.print(DHT.humidity);
     Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(DHT.temperature); 
-    Serial.println(" C  ");
-    
+    Serial.print("Temperature = ");
+    Serial.print(DHT.temperature);
+    Serial.println("C  ");
+
+    lcd.setCursor(0,0); 
+    lcd.print("Temp: ");
+    lcd.print(DHT.temperature);
+    lcd.print((char)223);
+    lcd.print("C");
+    lcd.setCursor(0,1);
+    lcd.print("Humidity: ");
+    lcd.print(DHT.humidity);
+    lcd.print("%");
+    delay(1000);
+      
     delay(5000);//Wait 5 seconds before accessing sensor again.
  
   //Fastest should be once every two seconds.
