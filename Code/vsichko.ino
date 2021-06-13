54f5e5fba405f8b5a3319ecfd8736e996d81cf28
Janet Hadjiangelova <janet.hadjiangelova@gmail.com>
2021-06-13 17:33:27 +0300

    blinking led
    added function

--- Code/vsichko.ino
+++ Code/vsichko.ino
@@ -13,9 +13,10 @@ dht DHT;
 
 int water_val = 0;
 int water_lvl = 0;
-int redLED = 9;
-int minlvl = 10;
-int maxlvl = 90;
+int water_redLED = 9;
+int water_minlvl = 10;
+int water_maxlvl = 90;
+int i = 0;
 
 int pump = 0;
 int lvl=0;
@@ -25,8 +26,8 @@ void setup(){
   pinMode(water_pin, INPUT);
   Serial.begin(9600);
   
-  pinMode(redLED, OUTPUT);
-  digitalWrite(redLED, LOW);
+  pinMode(water_redLED, OUTPUT);
+  digitalWrite(water_redLED, LOW);
 
   pinMode(relay_pin, OUTPUT);
   digitalWrite(relay_pin, HIGH);
@@ -42,8 +43,6 @@ void loop(){
   lcd.clear(); 
   delay(100);
   
-
-  
 //Start of Water Level Sensor
 
   water_val = analogRead(water_pin);
@@ -54,67 +53,37 @@ void loop(){
   
   lcd.setCursor(0,0);
   if (level <= 0) {
-    Serial.println("Water Level: Empty");
+      Serial.println("Water Level: Empty");
       lcd.print("WL Empty");
-      digitalWrite(redLED, HIGH);
+      digitalWrite(water_redLED, HIGH);
       pump=pump+1;
   }
-  else if (level > 0 && level <= minlvl) {
-    Serial.println("Water Level: ");
-    Serial.println(water_lvl);
-    Serial.print(lvl);
-    lcd.print("WL ");
-    lcd.print(water_lvl);
-    lcd.print(" %");
-     digitalWrite(redLED, HIGH);
-      delay(700);
-      digitalWrite(redLED, LOW);
-      delay(700);
-      digitalWrite(redLED, HIGH);
-      delay(700);
-      digitalWrite(redLED, LOW);
-      delay(700);
-      digitalWrite(redLED, HIGH);
-      delay(700);
+  else if (level > 0 && level <= water_minlvl) {
+      Serial.println("Water Level: ");
+      Serial.println(water_lvl);
+      Serial.print(lvl);
+      lcd.print("WL ");
+      lcd.print(water_lvl);
+      lcd.print(" %");
+      blinking_led(5,250);
   }
-  else if (level > minlvl && level <= maxlvl) {
-    lcd.print("WL ");
-    lcd.print(water_lvl);
-    lcd.print(" %");
-    Serial.println("Water Level: ");
-    Serial.println(water_lvl);
-    Serial.println(lvl);
-    digitalWrite(redLED, LOW);
+  else if (level > water_minlvl && level <= water_maxlvl) {
+      lcd.print("WL ");
+      lcd.print(water_lvl);
+      lcd.print(" %");
+      Serial.println("Water Level: ");
+      Serial.println(water_lvl);
+      Serial.println(lvl);
+      digitalWrite(water_redLED, LOW);
   }
-  else if (level > maxlvl) {
-    lcd.print("WL ");
-    lcd.print(water_lvl);
-    lcd.print(" %");
-    Serial.println("Water Level: ");
-    Serial.println(water_lvl);
-    Serial.println(lvl);
-    digitalWrite(redLED, HIGH);
-    delay(50);
-    digitalWrite(redLED, LOW);
-    delay(50);
-    digitalWrite(redLED, HIGH);
-    delay(50);
-    digitalWrite(redLED, LOW);
-    delay(50);
-    digitalWrite(redLED, HIGH);
-    delay(50);
-    digitalWrite(redLED, LOW);
-    delay(50);
-    digitalWrite(redLED, HIGH);
-    delay(50);
-    digitalWrite(redLED, LOW);
-    delay(50);
-    digitalWrite(redLED, HIGH);
-    delay(50);
-    digitalWrite(redLED, LOW);
-    delay(50);
-    digitalWrite(redLED, HIGH);
-    delay(50);
+  else if (level > water_maxlvl) {
+      lcd.print("WL ");
+      lcd.print(water_lvl);
+      lcd.print(" %");
+      Serial.println("Water Level: ");
+      Serial.println(water_lvl);
+      Serial.println(lvl);
+      blinking_led(10,50);
   }
                              //End of Water Level Sensor
 
@@ -122,60 +91,67 @@ void loop(){
   lcd.setCursor(0,1);
   delay(100);
   float soil_value=0;
-  int dryness=0;
-  int moisture=0;
+  int soil_dryness=0;
+  int soil_moisture=0;
   soil_value = analogRead(soil_pin);
-  dryness = map(soil_value, 0, 1023, 0, 100);
-  moisture = 100 - dryness;
+  soil_dryness = map(soil_value, 0, 1023, 0, 100);
+  soil_moisture = 100 - soil_dryness;
   lcd.print("SM ");
-  lcd.print(moisture);
+  lcd.print(soil_moisture);
   lcd.print(" %");
   Serial.print("Moisture: ");
-  Serial.print(moisture);
+  Serial.print(soil_moisture);
   Serial.print("% \n");
   delay(3000);                   //End of Soil Moisture Sensor
 
 //Start of Water Pump
-if(pump==0){
-  if (moisture < 35){
-  digitalWrite(relay_pin, LOW);
-  delay(10000);
-  digitalWrite(relay_pin, HIGH);
-  pump=pump+1;
-  }
-}                              
-else{pump=pump+1;}
+  if(pump==0){
+    if (soil_moisture < 35){
+      digitalWrite(relay_pin, LOW);
+      delay(10000);
+      digitalWrite(relay_pin, HIGH);
+      pump=pump+1;
+    }
+  }else{pump=pump+1;}
   if (pump>=5){
-  pump=0;}                      //End of Water Pump
- 
-  lcd.setCursor(0,0); 
-  lcd.clear();
-  lcd.setCursor(0,1); 
-  lcd.clear();
-  delay(100);
+      pump=0;
+  }                      //End of Water Pump
+   
+    lcd.setCursor(0,0); 
+    lcd.clear();
+    lcd.setCursor(0,1); 
+    lcd.clear();
+    delay(100);
 
 //Start of Temperature-Humidity Sensor
-    DHT.read11(dht_apin);
+  DHT.read11(dht_apin);
 
-    Serial.print("Humidity = ");
-    Serial.print(DHT.humidity);
-    Serial.print("%  ");
-    Serial.print("Temperature = ");
-    Serial.print(DHT.temperature);
-    Serial.println("C  ");
+  Serial.print("Humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.print("%  ");
+  Serial.print("Temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.println("C  ");
 
-    lcd.setCursor(0,0); 
-    lcd.print("T ");
-    lcd.print(DHT.temperature);
-    lcd.print((char)223);
-    lcd.print("C");
-    lcd.setCursor(0,1);
-    lcd.print("H ");
-    lcd.print(DHT.humidity);
-    lcd.print("%");
-    delay(1000);
+  lcd.setCursor(0,0); 
+  lcd.print("T ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("H ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(1000);
       
-    delay(3000);//Wait 3 seconds before accessing sensor again.
+  delay(3000);//Wait 3 seconds before accessing sensor again.
   //Fastest should be once every two seconds.
                           //End of Temperature-Humidity Sensor
+}
+
+void blinking_led(int repeat, int wait){
+  for(i = 0; i <= repeat; i = i + 1)    
+    digitalWrite(water_redLED, HIGH);
+    delay(wait);
+    digitalWrite(water_redLED, LOW);
 }
